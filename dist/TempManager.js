91a94cef08fb2fda0cce3888d3f7b3bf43f1481e
Hadar Alon <hadaralon3@gmail.com>
2019-07-18 19:13:40 +0300

    need finish

--- dist/TempManager.js
+++ dist/TempManager.js
@@ -8,6 +8,11 @@ class TempManager {
 
     async getDataFromDB() {
         let data = await $.get(`/cities`)
+        let time = data.forEach(a => {a.updateAt});
+       
+        console.log(time);
+        
+        
             this.cityData = data
     }
     
@@ -22,6 +27,7 @@ class TempManager {
      saveCity(cityName) {
         for ( let city of this.cityData) {
                 if (city.name == cityName ) {
+                    // console.log(city)
                     $.post("/city", city )
                 }
             }
